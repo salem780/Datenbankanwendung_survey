fa95a71fdd598dee59bfc94ad1d9802e5c9b8289
Piet123456 <50941533+Piet123456@users.noreply.github.com>
2020-04-07 11:39:22 +0200

    Delete rating.sql

--- sql/rating.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Rating (
-    FOREIGN KEY (MNR) REFERENCES Student (MNR),
-    FOREIGN KEY (ID) REFERENCES Question (ID),
-    FOREIGN KEY (s_token) REFERENCES Question (s_token)
-    a_value INT CHECK (a_value >=1 AND a_value <=5),
-    CONSTRAINT PK_Rating PRIMARY KEY (MNR, ID,s_token)
-)
