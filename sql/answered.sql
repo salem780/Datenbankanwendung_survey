312fc61d74ba7cb261db004cde66aad235e84fed
Piet123456 <50941533+Piet123456@users.noreply.github.com>
2020-04-07 11:38:55 +0200

    Delete answered.sql

--- sql/answered.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Answered (
-    FOREIGN KEY (MNR) REFERENCES Student (MNR),
-    FOREIGN KEY (s_token) REFERENCES Survey (s_token),
-    comment VARCHAR (100),
-    status BIT NOT NULL,
-    CONSTRAINT PK_Answered PRIMARY KEY (MNR, s_token)
-)
