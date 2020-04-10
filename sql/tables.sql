af23162a8c175f6b8cef3d9b7e6e5ed5c1a6d6f1
SunnyLea <48595764+SunnyLea@users.noreply.github.com>
2020-04-10 15:44:05 +0200

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE survey(
 s_token CHAR(4) PRIMARY KEY,
 s_title VARCHAR (32) NOT NULL UNIQUE,
 username VARCHAR (32),
-FOREIGN KEY (username) REFERENCES surveyor (username)
+FOREIGN KEY (username) REFERENCES surveyor (username) on delete cascade on update cascade
 );
 
 
@@ -15,8 +15,8 @@ password VARCHAR (32) NOT NULL
 CREATE TABLE answered (
     MNR CHAR(8),
     s_token CHAR(4),
-    FOREIGN KEY (MNR) REFERENCES Student (MNR),
-    FOREIGN KEY (s_token) REFERENCES Survey (s_token),
+    FOREIGN KEY (MNR) REFERENCES Student (MNR) on delete cascade on update cascade,
+    FOREIGN KEY (s_token) REFERENCES Survey (s_token) on delete cascade on update cascade,
     comment VARCHAR (256),
     status BIT NOT NULL,
     CONSTRAINT PK_Answered PRIMARY KEY (MNR, s_token)
@@ -31,25 +31,27 @@ CREATE TABLE Rating (
     MNR CHAR(4),
     ID INT,
     s_token CHAR(4),
-    FOREIGN KEY (MNR) REFERENCES Student (MNR),
-    FOREIGN KEY (ID) REFERENCES Question (ID),
-    FOREIGN KEY (s_token) REFERENCES Question (s_token),
+    FOREIGN KEY (MNR) REFERENCES Student (MNR) on delete cascade on update cascade,
+    FOREIGN KEY (ID) REFERENCES Question (ID) on delete cascade on update cascade,
+    FOREIGN KEY (s_token) REFERENCES Question (s_token) on delete cascade on update cascade,
     a_value INT CHECK (a_value >=1 AND a_value <=5),
     CONSTRAINT PK_Rating PRIMARY KEY (MNR, ID,s_token)
 );
+
 
 CREATE TABLE student (
 MNR CHAR(8) PRIMARY KEY,
 student_name VARCHAR (32) NOT NULL,
 c_token CHAR(8),
-FOREIGN KEY (c_token) REFERENCES course (c_token)
+FOREIGN KEY (c_token) REFERENCES course (c_token) on delete cascade on update cascade
 );
-FOREIGN KEY(s_token) REFERENCES survey(s_token),
+
 
 CREATE TABLE Question (
 ID INT,
 Text VARCHAR(256),
 s_token CHAR(4),
+FOREIGN KEY (s_token) REFERENCES survey(s_token) on delete cascade on update cascade,
 CONSTRAINT PK_Question PRIMARY KEY (ID, s_token)
 );
 
@@ -57,7 +59,7 @@ CONSTRAINT PK_Question PRIMARY KEY (ID, s_token)
 CREATE TABLE Activation (
 c_token CHAR(8),
 s_token CHAR(4),
-FOREIGN KEY(c_token) REFERENCES course(c_token) ,
-FOREIGN KEY(s_token) REFERENCES survey(s_token),
+FOREIGN KEY(c_token) REFERENCES course(c_token) on delete cascade on update cascade,
+FOREIGN KEY(s_token) REFERENCES survey(s_token) on delete cascade on update cascade,
 CONSTRAINT PK_Activation PRIMARY KEY (c_token, s_token )
 );
