16ae45d00085d6383909734dab0577df12897d22
= <=>
2020-04-13 12:22:37 +0200

    changed overview page

--- surveyor_logged.php
+++ surveyor_logged.php
@@ -7,8 +7,10 @@
   </head>
   <body>
   <h1>Hallo XXX</h1>
-  <a href="create_survey.php">Fragebogen erstellen</a> <br>
-  <a href="edit_survey.php">Fragebogen bearbeiten</a> <br>
+  <a href="create_survey.php">Fragebogen erstellen</a> <br> <br>
+  <a href="delete_survey.php">Fragebogen löschen</a> <br>
+  <a href="copy_survey.php">Bestehenden Fragebogen kopieren</a> <br>
+  <a href="delete_or_add_questions.php">Fragen löschen oder hinzufügen</a> <br> <br>
   <a href="rating.php">Fragebogen auswerten</a>
   </body>
 </html>
