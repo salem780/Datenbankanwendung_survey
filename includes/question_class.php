2aa19b61f9db8dcef6fcf81ef1ccb55eab4a7a4f
Datenbankanwendung_survey <salinamoser@web.de>
2020-04-28 18:38:19 +0200

    avg and min works

--- includes/question_class.php
+++ includes/question_class.php
@@ -7,7 +7,7 @@ include_once 'db_connection.php'; // $db
 //$c_token = $_SESSION["coursetoken"];
 //echo $c_token;
 
-class Frageerg {
+class FrageErgebnis {
 public $id;
 public $anzantworten;
 public $durchschnitt;
@@ -30,7 +30,9 @@ public function __construct ($s_token, $c_token)
 $this->s_token = $s_token;
 $this->c_token = $c_token;
 $db = new mysqli("localhost", "root", "", "survey");
+$this->get_Comments($db);
 $this->lade_punkteergebnisse($db);
+
 }
 //public function setId ($id){
 //$this->id = $id;
@@ -58,28 +60,39 @@ $list .= "<p>" . $this->comments[$i] . "</p> <p> </p>";
 }
 return $list;
 }
-/*
-protected function lade_punkteergebnisse ($db,$id){
 
-$this->ergebnisse = array();
-$sql = "select id, avg(a_value) as avg, min(a_value) as min, max(a_value) as max, count(*) as anz from rating r, answered a, student s WHERE r.mnr= a.mnr AND a.mnr= s.mnr AND s.kurs= '" . $this->c_token . "' and  r.s_token = '" . $this->s_token . "' ;";
+protected function lade_punkteergebnisse ($db)
+
+{$this->ergebnisse = array();
+
+$sql = "select id, avg(a_value) as avg, min(a_value) as min from rating r, answered a, student s WHERE r.mnr = a.mnr AND a.mnr = s.mnr AND c_token = '" . $this->c_token . "'AND a.s_token = '" . $this->s_token . "' group by id order by id;";
+$result = mysqli_query ($db, $sql);
+
+
+
+
+
+while ($row = mysqli_fetch_assoc($result))
+{
+
+
 $frageerg = new FrageErgebnis;
-$frageerg->anzantworten = $row["anz"];
 $frageerg->durchschnitt = $row["avg"];
 $frageerg->minimum = $row["min"];
-$frageerg->maximum = $row["max"];
-$this->ergebnisse[] = $frageerg;
-}*/
+$this->ergebnisse[]= $frageerg;
+
+}
+
+
+}
+
+
+
 
 public function erg_fuer_frage ($id)
 { return $this->ergebnisse[$id];
-echo $this->ergebnisse[$id];
-}
 
 }
 
-
-
-
-
+}
 
